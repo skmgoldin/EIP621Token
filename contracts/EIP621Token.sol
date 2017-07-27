ec6cd480e9fa59d90a4282e3f99d2cf117e48597
Mike Goldin <mike.goldin@consensys.net>
2017-07-27 13:18:52 -0400

    Initial commit

---
+++ contracts/EIP621Token.sol
@@ -0,0 +1,44 @@
+pragma solidity 0.4.13;
+
+import "tokens/HumanStandardToken.sol";
+
+
+contract EIP621Token is HumanStandardToken {
+  
+    function EIP621Token(
+        uint256 _initialAmount,
+        string _tokenName,
+        uint8 _decimalUnits,
+        string _tokenSymbol
+    ) HumanStandardToken(
+        _initialAmount,
+        _tokenName,
+        _decimalUnits,
+        _tokenSymbol
+    ) 
+    {}
+
+    function increaseSupply(uint value, address to) public {
+        totalSupply = safeAdd(totalSupply, value);
+        balances[to] = safeAdd(balances[to], value);
+
+        Transfer(0, to, value);
+    }
+
+    function safeAdd(uint a, uint b) internal returns (uint) {
+        require(a + b >= a);
+        return a + b;
+    }
+
+    function decreaseSupply(uint value, address from) public {
+        balances[from] = safeSub(balances[from], value);
+        totalSupply = safeSub(totalSupply, value);
+
+        Transfer(from, 0, value);
+    }
+
+    function safeSub(uint a, uint b) internal returns (uint) {
+        require(a >= b);
+        return a - b;
+    }
+}
