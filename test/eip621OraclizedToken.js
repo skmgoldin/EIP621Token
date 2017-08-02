2be625e8bbbfe4269859e93dd8e425aaee1a0332
Kareem <kareem315@gmail.com>
2017-08-02 13:35:51 -0400

    Add decrease tests for oracle token

--- test/eip621OraclizedToken.js
+++ test/eip621OraclizedToken.js
@@ -39,6 +39,28 @@ contract(`EIP621OraclizedToken`, (accounts) => {
 
       assert.strictEqual((await recipientBalance).toString(), expectedBalance)
       assert.strictEqual((await totalSupply).toString(), expectedSupply)
+    }) 
+
+    it('Should increase supply by 10 then decrease by 4', async () => {
+        const amountIncrease = 10
+        const amountDecrease = 6
+        const amountFinal = amountIncrease - amountDecrease
+        const from = accounts[9]
+
+        const instance = await EIP621OraclizedToken.new(initialAmount,
+        tokenName, decimalUnits, tokenSymbol, supplyOracle)
+        
+        await asOracle(instance.increaseSupply, amountIncrease, from)
+        await asOracle(instance.decreaseSupply, amountDecrease, from)
+        
+        const recipientBalance = instance.balanceOf.call(from)
+        const expectedBalance = amountFinal.toString();
+
+        const totalSupply = instance.totalSupply.call()
+        const expectedSupply = (initialAmount + (amountIncrease - amountDecrease)).toString()
+
+        assert.strictEqual((await recipientBalance).toString(), expectedBalance)
+        assert.strictEqual((await totalSupply).toString(), expectedSupply)
     })
   })
 
@@ -56,6 +78,28 @@ contract(`EIP621OraclizedToken`, (accounts) => {
       } catch(err) { 
         assert(isEVMException(err))
       }
+    })
+
+    it('Should increase the supply by 10 and fail to decrease it by 11', async () => {
+        const amountIncrease = 10
+        const amountDecrease = 11
+        const from = accounts[9]
+
+        const instance = await EIP621OraclizedToken.new(initialAmount,
+                tokenName, decimalUnits, tokenSymbol, supplyOracle) 
+        
+        try {
+            await instance.increaseSupply(amountIncrease, from, {from: accounts[1]})
+        } catch(err) {
+            console.log(err);
+        }
+
+        try {
+            await instance.decreaseSupply(amountDecrease, from)
+        } catch(err) {
+            assert(isEVMException(err)) 
+        }
+
     })
   })
 })
